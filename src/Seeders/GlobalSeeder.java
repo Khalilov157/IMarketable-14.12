44af6691e4cfb0170a78d0a8dff8a3ddb13a1285
Khalilov157 <evilcrewstudios@gmail.com>
2022-12-14 08:36:10 +0400

    Initial commit comments

--- src/Seeders/GlobalSeeder.java
+++ src/Seeders/GlobalSeeder.java
@@ -129,5 +129,23 @@ public class GlobalSeeder {
         return false;
     }
 
+    /*
+
+ + Satislar
+ + Mehsullar
+ + Satis elave etmek - satis elave edilerken hansi mehsullardan hansi sayda satis oldugu gonderilir
+ + Satisdan mehsulun geri qaytarilmasi
+ - Umumi satisin geri qaytarilmasi
+ - Verilen tarix araligina gore hemin tarix araligina olan satislarin qaytarilmasi
+ - Verilen bir tarixe gore hemin tarix (il,ay,gun) ucun olan satislarin qaytarilmasi
+ - Verilmis mebleg araligina gore edilmis satislarin qaytarilmasi
+ + Verilmis nomreye esasen satisin qaytarilmasi
+ - Yeni mehsul elave etmek
+ - Mehsulun adini,sayini ve meblegini,categoriyasini deyismek (code-a gore tapilacaq)
+ - Verilmis kateqoriyaya esasen hemin kateqoriyada olan mehsullarin qaytarilmasi
+ - Verilmis qiymet araligina esasen hemin araliqda olan mehsullarin qaytarilmasi
+ - Verilmis ada esasen mehsullarin search edilib qaytarilmasi
+     */
+
 
 }
